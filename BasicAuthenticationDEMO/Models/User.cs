e5a38ccea86d16e0b0c7b7f2abf1b65807d27a28
turkayurkmez <turkay@turkayurkmez.com>
2019-11-23 19:56:46 +0300

    User updated

--- BasicAuthenticationDEMO/Models/User.cs
+++ BasicAuthenticationDEMO/Models/User.cs
@@ -8,9 +8,14 @@ namespace BasicAuthenticationDEMO.Models
     public class User
     {
         public Guid Id { get; set; }
+
         public string Name { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
+
+        
+
 
     }
 }
