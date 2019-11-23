8eb3840688b448cd552dbe055595781429d47ff9
turkayurkmez <turkay@turkayurkmez.com>
2019-11-23 20:00:54 +0300

    User changed

--- BasicAuthenticationDEMO/Models/User.cs
+++ BasicAuthenticationDEMO/Models/User.cs
@@ -13,8 +13,9 @@ namespace BasicAuthenticationDEMO.Models
         public string LastName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
 
-        
+
 
 
     }
